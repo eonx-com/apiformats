a5080d5c4ad2add0b32d717747f8f151c0006a69
NatePage <nathan.page@loyaltycorp.com.au>
2018-02-02 10:42:53 +1100

    Make lctools green.

--- tests/Stubs/SerializableInterfaceStub.php
+++ tests/Stubs/SerializableInterfaceStub.php
@@ -10,7 +10,7 @@ class SerializableInterfaceStub implements SerializableInterface
     /**
      * Specify data which should be serialized to JSON
      * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * @return array data which can be serialized by <b>json_encode</b>,
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
