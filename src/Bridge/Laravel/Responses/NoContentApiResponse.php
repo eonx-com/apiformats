7fcbe7e1d574c80ddc9574825f591a225f221766
Mitchell Macpherson <mitchell@loyaltycorp.com.au>
2018-06-01 13:44:38 +1000

    Updated NoContentApiResponse class phpDocs

--- src/Bridge/Laravel/Responses/NoContentApiResponse.php
+++ src/Bridge/Laravel/Responses/NoContentApiResponse.php
@@ -3,13 +3,10 @@ declare(strict_types=1);
 
 namespace EoneoPay\ApiFormats\Bridge\Laravel\Responses;
 
-/**
- * This class has for purpose to easily customise status code and/or headers for given content.
- */
 class NoContentApiResponse extends FormattedApiResponse
 {
     /**
-     * FormattedApiResponse constructor.
+     * NoContentApiResponse constructor.
      *
      * @param int|null $statusCode
      * @param mixed[]|null $headers
